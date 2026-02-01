43bf486224e63b504e6f0459f0f96b6b93b4bc5a
Luckytucker1 <elockhart430@yahoo.com>
2026-02-01 16:14:15 -0500

    writing diologue

--- scripts/items/Key.cs
+++ scripts/items/Key.cs
@@ -4,33 +4,33 @@ using System;
 
 public partial class Key : Node3D, IClickable
 {
-    bool isActive = true;
-    public bool IsActive { get => isActive; set => isActive = value; }
-    
-    public void LeftClick(Vector3? position)
-    {
-        DU.Log("Clicked");
-        PickUpKey();
-    }
+	bool isActive = true;
+	public bool IsActive { get => isActive; set => isActive = value; }
+	
+	public void LeftClick(Vector3? position)
+	{
+		DU.Log("Clicked");
+		PickUpKey();
+	}
 
-    public void LeftRelease(Vector3? position)
-    {
-        
-    }
+	public void LeftRelease(Vector3? position)
+	{
+		
+	}
 
-    public void RightClick(Vector3? position)
-    {
-        
-    }
+	public void RightClick(Vector3? position)
+	{
+		
+	}
 
-    public void RightRelease(Vector3? position)
-    {
-        
-    }
+	public void RightRelease(Vector3? position)
+	{
+		
+	}
 
-    private void PickUpKey()
-    {
-        Inventory.Instance.HasKey = true;
-        QueueFree();
-    }
+	private void PickUpKey()
+	{
+		Inventory.Instance.HasKey = true;
+		QueueFree();
+	}
 }
