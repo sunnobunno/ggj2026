0a2e9dc4f33696f00ae278484252bd442c1a992a
Luckytucker1 <elockhart430@yahoo.com>
2026-02-01 15:42:54 -0500

    staged people and masks

--- scripts/door/DoorController.cs
+++ scripts/door/DoorController.cs
@@ -4,59 +4,59 @@ using System;
 
 public partial class DoorController : Node3D, IClickable
 {
-    bool isActive = true;
-    public bool IsActive { get => isActive; set => isActive = value; }
+	bool isActive = true;
+	public bool IsActive { get => isActive; set => isActive = value; }
 
-    public override void _Process(double delta)
-    {
-        
-    }
+	public override void _Process(double delta)
+	{
+		
+	}
 
-    private void OpenDoor()
-    {
-        var finalRotation = new Vector3(0f, Mathf.DegToRad(-85f), 0f);
-        
-        var tween = GetTree().CreateTween();
-        tween.SetTrans(Tween.TransitionType.Spring);
-        tween.SetEase(Tween.EaseType.Out);
-        tween.TweenProperty(this, "rotation", finalRotation, 1f);
-    }
+	private void OpenDoor()
+	{
+		var finalRotation = new Vector3(0f, Mathf.DegToRad(-85f), 0f);
+		
+		var tween = GetTree().CreateTween();
+		tween.SetTrans(Tween.TransitionType.Spring);
+		tween.SetEase(Tween.EaseType.Out);
+		tween.TweenProperty(this, "rotation", finalRotation, 1f);
+	}
 
-    private void CloseDoor()
-    {
-        var finalRotation = new Vector3(0f, Mathf.DegToRad(0f), 0f);
+	private void CloseDoor()
+	{
+		var finalRotation = new Vector3(0f, Mathf.DegToRad(0f), 0f);
 
-        var tween = GetTree().CreateTween();
-        tween.SetTrans(Tween.TransitionType.Spring);
-        tween.SetEase(Tween.EaseType.Out);
-        tween.TweenProperty(this, "rotation", finalRotation, 1f);
-    }
-
-
+		var tween = GetTree().CreateTween();
+		tween.SetTrans(Tween.TransitionType.Spring);
+		tween.SetEase(Tween.EaseType.Out);
+		tween.TweenProperty(this, "rotation", finalRotation, 1f);
+	}
 
 
 
-    public void LeftClick(Vector3? position)
-    {
-        if (Inventory.Instance.HasKey)
-        {
-            OpenDoor();
-            Inventory.Instance.HasKey = false;
-        }
-    }
 
-    public void LeftRelease(Vector3? position)
-    {
-        
-    }
 
-    public void RightClick(Vector3? position)
-    {
-        
-    }
+	public void LeftClick(Vector3? position)
+	{
+		if (Inventory.Instance.HasKey)
+		{
+			OpenDoor();
+			Inventory.Instance.HasKey = false;
+		}
+	}
 
-    public void RightRelease(Vector3? position)
-    {
-        
-    }
+	public void LeftRelease(Vector3? position)
+	{
+		
+	}
+
+	public void RightClick(Vector3? position)
+	{
+		
+	}
+
+	public void RightRelease(Vector3? position)
+	{
+		
+	}
 }
